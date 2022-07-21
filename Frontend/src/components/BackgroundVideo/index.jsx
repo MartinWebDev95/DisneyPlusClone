1c4807fde00334c4b463ac3087022aebf87f3676
MartinWebDev95 <jamll.muro@gmail.com>
2022-07-21 22:04:37 +0200

    Añadir opacidad al background en modo responsive

--- Frontend/src/components/BackgroundVideo/index.jsx
+++ Frontend/src/components/BackgroundVideo/index.jsx
@@ -1,22 +1,13 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { VideoStyled, ImageStyled } from './styles';
+import { VideoStyled, ImageStyled, DivOpacity } from './styles';
 
 function BackgroundVideo({ bgVideo, posterImage }) {
   const [opacityImage, setOpacityImage] = useState(false);
 
-  // window.addEventListener('scroll', () => {
-  //   const divVideoImage = document.querySelector('#container__video-image');
-
-  //   divVideoImage.style.opacity = 1;
-
-  //   while (divVideoImage.style.opacity > 0.2) {
-  //     const opa = divVideoImage.style.opacity;
-  //   }
-  // });
-
   return (
     <div id="container__video-image">
+      <DivOpacity />
       <VideoStyled autoPlay playsInline muted onEnded={() => setOpacityImage(true)}>
         <source src={bgVideo} type="video/mp4" />
       </VideoStyled>
--- Frontend/src/components/BackgroundVideo/styles.js
+++ Frontend/src/components/BackgroundVideo/styles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const VideoStyled = styled.video`
-    width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: -1;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
 `;
 
 const ImageStyled = styled.img`
@@ -21,4 +21,19 @@ const ImageStyled = styled.img`
   transition: all .3s ease-in-out;
 `;
 
-export { VideoStyled, ImageStyled };
+const DivOpacity = styled.div`
+  display: none;
+
+  @media screen and (max-width: 468px){
+    display: block;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    z-index: 0;
+    top: 0;
+    left: 0;
+    background-image: linear-gradient(to bottom, rgba(26, 29, 41, 0), rgba(26, 29, 41, 1), rgba(26, 29, 41, 0), rgba(26, 29, 41, 0)); 
+  }
+`;
+
+export { VideoStyled, ImageStyled, DivOpacity };
