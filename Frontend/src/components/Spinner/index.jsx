29c29e2f11048924dfce8bfd003ec076d6d145bb
MartinWebDev95 <jamll.muro@gmail.com>
2022-11-08 13:52:11 +0100

    Cambiar los estilos del componente Spinner

--- Frontend/src/components/Spinner/index.jsx
+++ Frontend/src/components/Spinner/index.jsx
@@ -2,20 +2,22 @@ import './styles.css';
 
 function Spinner() {
   return (
-    <div className="sk-fading-circle">
-      <div className="sk-circle1 sk-circle" />
-      <div className="sk-circle2 sk-circle" />
-      <div className="sk-circle3 sk-circle" />
-      <div className="sk-circle4 sk-circle" />
-      <div className="sk-circle5 sk-circle" />
-      <div className="sk-circle6 sk-circle" />
-      <div className="sk-circle7 sk-circle" />
-      <div className="sk-circle8 sk-circle" />
-      <div className="sk-circle9 sk-circle" />
-      <div className="sk-circle10 sk-circle" />
-      <div className="sk-circle11 sk-circle" />
-      <div className="sk-circle12 sk-circle" />
-    </div>
+    <main className="container">
+      <div className="sk-fading-circle">
+        <div className="sk-circle1 sk-circle" />
+        <div className="sk-circle2 sk-circle" />
+        <div className="sk-circle3 sk-circle" />
+        <div className="sk-circle4 sk-circle" />
+        <div className="sk-circle5 sk-circle" />
+        <div className="sk-circle6 sk-circle" />
+        <div className="sk-circle7 sk-circle" />
+        <div className="sk-circle8 sk-circle" />
+        <div className="sk-circle9 sk-circle" />
+        <div className="sk-circle10 sk-circle" />
+        <div className="sk-circle11 sk-circle" />
+        <div className="sk-circle12 sk-circle" />
+      </div>
+    </main>
   );
 }
 
