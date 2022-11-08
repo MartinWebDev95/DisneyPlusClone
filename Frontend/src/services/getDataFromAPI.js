b1a9199cdac5da779628bf9010727a0af84bb8a1
MartinWebDev95 <jamll.muro@gmail.com>
2022-11-08 13:52:53 +0100

    Cambiar algunos endpoints de la API para obtener las películas o series

--- Frontend/src/services/getDataFromAPI.js
+++ Frontend/src/services/getDataFromAPI.js
@@ -1,11 +1,11 @@
 const getNewItemsDisney = async (type) => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=release_date.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=release_date.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
 
 const getItemsForGenre = async (type, genre) => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=release_date.desc&with_genres=${genre}&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=release_date.desc&with_genres=${genre}&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
@@ -41,31 +41,31 @@ const getSearchSeries = async (keyword) => {
 };
 
 const getItemsFromBrand = async (type, brand) => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=${brand}&sort_by=vote_average.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=${brand}&sort_by=vote_average.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
 
 const getMostPopularMoviesPixar = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=3&sort_by=vote_average.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=3&sort_by=vote_average.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
 
 const getMoviesWaltDisneyAnimations = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=6125&sort_by=vote_average.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=6125&sort_by=vote_average.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
 
 const getMoviesWaltDisney90s = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=3475&with_companies=2&sort_by=vote_average.desc&release_date.gte=1990-01-01&release_date.lte=1999-01-01&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_companies=3475&with_companies=2&sort_by=vote_average.desc&release_date.gte=1990-01-01&release_date.lte=1999-01-01&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
 
 const getMoviesWaltDisneyActionAdventure = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_genres=12&with_companies=2&sort_by=vote_average.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&with_genres=12&with_companies=2&sort_by=vote_average.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
@@ -77,7 +77,7 @@ const getItemsCollection = async (collection) => {
 };
 
 const getMostPopularsMoviesDisney = async () => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=popularity.desc&language=es-ES&page=1`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/movie?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&watch_region=ES&sort_by=popularity.desc&language=es-ES&page=1`);
   const data = await response.json();
   return data.results;
 };
@@ -89,7 +89,7 @@ const getSeasonEpisodes = async (id, seasonNumber) => {
 };
 
 const getItemsFromDisney = async (type, page, genre = '') => {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}/?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&with_genres=${genre}&watch_region=ES&sort_by=release_date.desc&language=es-ES&page=${page}`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}discover/${type}?api_key=${import.meta.env.VITE_API_KEY}&with_watch_providers=337&with_genres=${genre}&watch_region=ES&sort_by=release_date.desc&language=es-ES&page=${page}`);
   const data = await response.json();
   return data.results;
 };
