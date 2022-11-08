564089f220df2017d1ff905b91380a6cf81d7d40
MartinWebDev95 <jamll.muro@gmail.com>
2022-11-08 13:49:26 +0100

    Creación de un custom hook para obtener las horas y minutos de una serie o película

--- Frontend/src/hooks/useHoursAndMinutes.js
+++ Frontend/src/hooks/useHoursAndMinutes.js
@@ -1,5 +1,6 @@
-export default function getHoursAndMinutes(minutes) {
+export default function useHoursAndMinutes(minutes) {
   const hours = Math.floor(minutes / 60);
   const minutesLeft = minutes % 60;
+
   return { hours, minutesLeft };
 }
