dba4637cbcc990f4866ab2c851fb73e066ba76dd
MartinWebDev95 <jamll.muro@gmail.com>
2022-11-08 09:57:39 +0100

    Refactorizar el custom hook useOpacity para que también controle la opacidad del fondo de detalles

--- Frontend/src/hooks/useOpacity.js
+++ Frontend/src/hooks/useOpacity.js
@@ -1,16 +1,27 @@
 /* eslint-disable no-unused-expressions */
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function useOpacity() {
   const [opacity, setOpacity] = useState(false);
+  const background = useRef(null);
 
-  const changeOpacity = () => {
+  const changeNavBarOpacity = () => {
     window.scrollY > 0 ? setOpacity(true) : setOpacity(false);
   };
 
-  window.addEventListener('scroll', changeOpacity);
+  const changeBackgroundOpacity = () => {
+    if (background.current && ((1.0 - (window.scrollY / 100)) > 0.2)) {
+      const scrollValue = (window.scrollY / 100).toFixed(1);
+      background.current.style.opacity = 1.0 - scrollValue;
+    }
+  };
 
-  return { opacity };
+  window.addEventListener('scroll', () => {
+    changeNavBarOpacity();
+    changeBackgroundOpacity();
+  });
+
+  return { opacity, background };
 }
 
 export default useOpacity;
