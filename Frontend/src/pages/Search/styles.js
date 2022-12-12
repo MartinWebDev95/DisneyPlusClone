ba15d213cf3cb6e9ac9bb143a6717411617b06e1
MartinWebDev95 <jamll.muro@gmail.com>
2022-12-12 13:18:37 +0100

    Modificar los estilos de la página Search

--- Frontend/src/pages/Search/styles.js
+++ Frontend/src/pages/Search/styles.js
@@ -11,8 +11,8 @@ const FormStyled = styled.form`
 
 const SearchField = styled.input`
   width: 100%;
-  padding: 2rem 4rem;
-  font-size: 2.8em;
+  padding: 1rem 2rem;
+  font-size: clamp(1.8em, 6vw, 2.8em); 
   color: #939498;
   font-weight: bold;
   background-color: rgb(75, 78, 90);
@@ -22,20 +22,23 @@ const SearchField = styled.input`
     color: #939498;
   }
 
-  @media screen and (max-width: 768px){
-    padding: 1rem 2rem;
-    font-size: 2.2em;
+  @media screen and (min-width: 768px){
+    padding: 2rem 4rem;
   }
 `;
 
 const Container = styled.section`
   width: 90%;
   margin: 0 auto;
-  margin-top: 15rem;
+  margin-top: 12rem;
 
   h1{
     color: #F9F9F9;
     font-size: 1.2em;
+  }
+
+  @media screen and (min-width: 768px){
+    margin-top: 15rem;
   }
 `;
 
