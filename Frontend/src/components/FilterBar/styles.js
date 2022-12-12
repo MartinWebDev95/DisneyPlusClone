e2f34fc4681dd13551134be6db40149d21fc8762
MartinWebDev95 <jamll.muro@gmail.com>
2022-12-12 13:03:20 +0100

    Modificar los estilos del componente FilterBar

--- Frontend/src/components/FilterBar/styles.js
+++ Frontend/src/components/FilterBar/styles.js
@@ -2,37 +2,36 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
   color: #DBDBDB;
-  font-size: 3em;
-
-  @media screen and (max-width: 468px){
-    padding: 0;
-    margin: 1rem;
-  }
+  font-size: clamp(2rem, 6vw, 3rem); 
+  margin: 0;
 `;
 
 const SectionFormStyled = styled.section`
   display: flex;
   align-items: center;
-  gap: 2rem;
   width: 100%;
-  padding-left: 5rem;
   position: fixed;
   z-index: 1;
   top: 0;
-  padding-top: 4.7rem;
+  padding-left: 0;
+  padding-top: 7rem;
+  padding-bottom: 3rem;
+  flex-direction: column;
+  gap: 0;
   
   ${(props) => (props.opacity === 'false'
     ? 'background-color: rgba(20, 22, 31, 0);'
     : 'background-color: rgba(20, 22, 31, 1);')};
 
-  @media screen and (max-width: 468px){
-    flex-direction: column;
-    padding-left: 0;
-    gap: 0;
+  @media screen and (min-width: 500px){
+    flex-direction: row;
+    gap: 2rem;
+    padding-left: 5rem;
   }
 `;
 
 const FormStyled = styled.form`
+  padding-top: 1rem;
   
   select{
     padding: .6rem;
@@ -40,20 +39,21 @@ const FormStyled = styled.form`
     background-color: #0F1017;
     color: #DBDBDB;
     border: none;
-    font-size: 1em;
+    font-size: clamp(.8em, 3vw, 1em); 
 
     &:hover{
       cursor: pointer;
     }
   }
 
-  @media screen and (max-width: 468px){
-    margin-bottom: 1rem;
+  @media screen and (min-width: 500px){
+    padding-top: 0rem;
   }
 `;
 
 const OptionStyled = styled.option`
   text-transform: uppercase;
+  font-size: clamp(.8em, 2.5vw, 1em);
 
   &:checked{
     background-color: rgb(100, 100, 100);
