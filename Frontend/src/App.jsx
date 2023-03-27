73022d984c40c62d8e3d14970db8423d1df5b3d9
Juan Antonio <jamll.muro@gmail.com>
2023-03-27 14:26:43 +0200

    Añadir el nuevo componente BrandPage a cada ruta que hace referencia a una brand

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,11 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import MyList from './pages/MyList';
 import Search from './pages/Search';
-import Disney from './pages/Disney';
-import Marvel from './pages/Marvel';
-import Star from './pages/Star';
-import StarWars from './pages/Star_Wars';
-import NationalGeographic from './pages/National_Geographic';
-import Pixar from './pages/Pixar';
 import Detail from './pages/Detail';
 import Movies from './pages/Movies';
 import Series from './pages/Series';
 import AuthLayout from './layout/AuthLayout';
+import BrandPage from './pages/BrandPage';
 
 function App() {
   return (
@@ -27,12 +22,12 @@ function App() {
           <Route path="tv" element={<Series />} />
           <Route path="movie/:id" element={<Detail type="movie" />} />
           <Route path="tv/:id" element={<Detail type="tv" />} />
-          <Route path="brand/disney" element={<Disney />} />
-          <Route path="brand/marvel" element={<Marvel />} />
-          <Route path="brand/pixar" element={<Pixar />} />
-          <Route path="brand/star" element={<Star />} />
-          <Route path="brand/star-wars" element={<StarWars />} />
-          <Route path="brand/national-geographic" element={<NationalGeographic />} />
+          <Route path="brand/disney" element={<BrandPage brand="disney" />} />
+          <Route path="brand/marvel" element={<BrandPage brand="marvel" />} />
+          <Route path="brand/pixar" element={<BrandPage brand="pixar" />} />
+          <Route path="brand/star" element={<BrandPage brand="star" />} />
+          <Route path="brand/star-wars" element={<BrandPage brand="starWars" />} />
+          <Route path="brand/national-geographic" element={<BrandPage brand="nationalGeographic" />} />
         </Route>
       </Routes>
     </div>
