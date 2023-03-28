35831cb6bf64fb1f40caddca2eff20b2dad9cce2
Juan Antonio <jamll.muro@gmail.com>
2023-03-28 11:52:46 +0200

    Añadir un nuevo archivo listGenres.js que contiene todos los géneros de series y películas

---
+++ Frontend/src/utils/listGenres.js
@@ -0,0 +1,26 @@
+const listGenres = {
+  movie: [
+    { '': 'Todas las películas' },
+    { 28: 'Acción' },
+    { 12: 'Aventura' },
+    { 35: 'Comedia' },
+    { 16: 'Animación' },
+    { 18: 'Drama' },
+    { 878: 'Ciencia Ficción' },
+    { 53: 'Thrillers' },
+    { 99: 'Documental' },
+  ],
+
+  tv: [
+    { '': 'Todas las series' },
+    { 10759: 'Acción y Aventura' },
+    { 35: 'Comedia' },
+    { 16: 'Animación' },
+    { 18: 'Drama' },
+    { 10765: 'Ciencia Ficción' },
+    { 9648: 'Thrillers' },
+    { 99: 'Docuserie' },
+  ],
+};
+
+export default listGenres;
