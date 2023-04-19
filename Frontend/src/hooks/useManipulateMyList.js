cb4e7e26f3188d29dd3c9554671cdb6c1a7aedc3
Juan Antonio <jamll.muro@gmail.com>
2023-04-19 14:19:43 +0200

    Cambiar el useEffect diviendolo en dos useEffect, uno que se encarga de obtener la info del item y otro que se encarga de comprobar si existe en la BD

--- Frontend/src/hooks/useManipulateMyList.js
+++ Frontend/src/hooks/useManipulateMyList.js
@@ -5,9 +5,7 @@ import { useMyList } from '../context/MyListContext';
 import { getItemDetail } from '../services/getDataFromAPI';
 
 function useManipulateMyList({ id, type }) {
-  let counter = 0;
-
-  const { user, loading } = useAuth();
+  const { user } = useAuth();
 
   const [itemDetail, setItemDetail] = useState({});
   const [isLoading, setLoading] = useState(true);
@@ -24,63 +22,63 @@ function useManipulateMyList({ id, type }) {
   } = useMyList();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-
     // Obtengo los detalles de la película o serie
     getItemDetail(id, type)
       .then((data) => {
         setItemDetail(data);
-        setLoading(false);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    // Compruebo si ya existe la pelicula o serie en la base de datos
+    getItemFromMyList(id)
+      .then((data) => {
+        setExists(data.ok);
+
+        // Guardo su id de mongo para luego utilizarlo para hacer las operaciones de CRUD
+        if (data.ok) {
+          setIdMongoose(data.itemMyList[0]._id);
+        }
       });
 
-    // Compruebo si ya existe la pelicula o serie en la base de datos
-    getItemFromMyList(id).then((data) => {
-      setExists(data.ok);
-
-      // Guardo su id de mongo para luego utilizarlo para hacer las operaciones de CRUD
-      if (data.ok) {
-        setIdMongoose(data.itemMyList[0]._id);
-      }
-    });
-
     // Compruebo si la pelicula o serie guardada en la base de datos pertenece al usuario actual
-    if (!loading) {
-      getAllItemsFromMyList({ userId: user?.id }).then((data) => {
+    getAllItemsFromMyList({ userId: user?.id })
+      .then((data) => {
         data.forEach((dataItem) => {
           if (dataItem.idItem === id) {
             setSelected(true);
-            counter += 1;
           }
         });
-
-        if (counter === 0) {
-          setSelected(false);
-        }
+      })
+      .finally(() => {
+        setLoading(false);
       });
-    }
-  }, [id, loading]);
+  }, [selected]);
 
   const handleSaveMyList = async () => {
     // Si la película o serie ya existe en la BD, se actualiza
-    // el subdocumento de usuarios de la película o serie y se
+    // el array de usuarios de la película o serie y se
     // añade el usuario actual
-    if (exists) {
-      updateItemFromMyList({ idItem: id, userId: user.id, idMongoose });
+    if (exists && !selected) {
+      await updateItemFromMyList({ idItem: id, userId: user.id, idMongoose });
+
       setSelected(true);
     } else {
-      addItemToMyList({
+      await addItemToMyList({
         idItem: id,
         posterPath: itemDetail.poster_path,
         type,
         userId: user.id,
       });
+
       setSelected(true);
     }
 
     // Si el botón está seleccionado quiere decir que está en la BD, por lo tanto,
     // se elimina el usuario actual de la colección de usuarios de la película o serie
     if (selected) {
-      deleteItemFromMyList({ idItem: id, userId: user.id, idMongoose });
+      await deleteItemFromMyList({ idItem: id, userId: user.id, idMongoose });
+
       setSelected(false);
     }
   };
