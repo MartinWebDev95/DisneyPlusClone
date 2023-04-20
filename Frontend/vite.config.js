8da6cce144a57c7b2d946b3b764e43e8a93e475d
Juan Antonio <jamll.muro@gmail.com>
2023-04-20 12:18:00 +0200

    Añadir el plugin vite-plugin-commonjs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCommonjs(),
+  ],
 });
