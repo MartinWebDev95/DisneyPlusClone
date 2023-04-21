4ba2183ed81fdc69bf6d255c3eab2895510b02eb
Juan Antonio <jamll.muro@gmail.com>
2023-04-21 12:53:40 +0200

    Añadir una nueva variable de entorno al servidor

--- Backend/passport.js
+++ Backend/passport.js
@@ -6,7 +6,7 @@ const passportConfiguration = () => {
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: process.env.CALLBACK_URL,
     },
     ((accessToken, refreshToken, profile, done) => {
       done(null, profile);
