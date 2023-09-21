a5e5556c6f464a57dac5df68b488a8a42f978843
Juan Antonio <jamll.muro@gmail.com>
2023-09-21 14:11:34 +0200

    Change the useManipulateMyList custom hook to add a movie or tv show to my list with the supabase functions

--- Frontend/src/hooks/useManipulateMyList.js
+++ Frontend/src/hooks/useManipulateMyList.js
@@ -1,24 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { useMyList } from '../context/MyListContext';
 import { getItemDetail } from '../services/getDataFromAPI';
+import addItemToDB from '../services/addItemToDB';
+import addItemToMyList from '../services/addItemToMyList';
 
 function useManipulateMyList({ id, type }) {
-  const { user } = useAuth();
+  const { currentUser } = useAuth();
 
   const [itemDetail, setItemDetail] = useState({});
   const [isLoading, setLoading] = useState(false);
   const [selected, setSelected] = useState(false);
-  const [exists, setExists] = useState(false);
-  const [idMongoose, setIdMongoose] = useState(null);
-
-  const {
-    addItemToMyList,
-    getItemFromMyList,
-    updateItemFromMyList,
-    deleteItemFromMyList,
-  } = useMyList();
 
   useEffect(() => {
     setLoading(true);
@@ -27,24 +19,6 @@ function useManipulateMyList({ id, type }) {
     getItemDetail(id, type)
       .then((data) => {
         setItemDetail(data);
-      });
-
-    // Compruebo si ya existe la pelicula o serie en la base de datos
-    getItemFromMyList({ userId: user?.id, idItem: id })
-      .then((data) => {
-        setExists(data.ok);
-
-        // Guardo su id de mongo para luego utilizarlo para hacer las operaciones de CRUD
-        if (data.ok) {
-          setIdMongoose(data.itemMyList[0]._id);
-        }
-
-        // Compruebo si la pelicula o serie guardada en la base de datos pertenece al usuario actual
-        if (data.belongsToUser) {
-          setSelected(true);
-        } else {
-          setSelected(false);
-        }
       })
       .finally(() => {
         setLoading(false);
@@ -52,31 +26,20 @@ function useManipulateMyList({ id, type }) {
   }, [id]);
 
   const handleSaveMyList = async () => {
-    // Si la película o serie ya existe en la BD, se actualiza
-    // el array de usuarios de la película o serie y se
-    // añade el usuario actual
-    if (exists && !selected) {
-      await updateItemFromMyList({ idItem: id, userId: user.id, idMongoose });
+    // Añadir la película o serie a la base de datos
+    const newItem = await addItemToDB({
+      idItem: id,
+      posterPath: itemDetail.poster_path,
+      type,
+    });
 
-      setSelected(true);
-    } else {
-      await addItemToMyList({
-        idItem: id,
-        posterPath: itemDetail.poster_path,
-        type,
-        userId: user.id,
-      });
+    // Relacionar la película o serie añadida con el usuario actual
+    await addItemToMyList({
+      idUser: currentUser.id,
+      idItem: newItem[0].id,
+    });
 
-      setSelected(true);
-    }
-
-    // Si el botón está seleccionado quiere decir que está en la BD, por lo tanto,
-    // se elimina el usuario actual de la colección de usuarios de la película o serie
-    if (selected) {
-      await deleteItemFromMyList({ idItem: id, userId: user?.id, idMongoose });
-
-      setSelected(false);
-    }
+    setSelected(true);
   };
 
   return {
