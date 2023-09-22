6862dbf40e8089fef34d37b9174fed915bafc32e
Juan Antonio <jamll.muro@gmail.com>
2023-09-22 11:28:51 +0200

    Change the useManipulateMyList custom hook to use the new getItemFromMyList function to update the setSelected state depending of the result

--- Frontend/src/hooks/useManipulateMyList.js
+++ Frontend/src/hooks/useManipulateMyList.js
@@ -4,6 +4,7 @@ import { useAuth } from '../context/AuthContext';
 import { getItemDetail } from '../services/getDataFromAPI';
 import addItemToDB from '../services/addItemToDB';
 import addItemToMyList from '../services/addItemToMyList';
+import getItemFromMyList from '../services/getItemFromMyList';
 
 function useManipulateMyList({ id, type }) {
   const { currentUser } = useAuth();
@@ -23,11 +24,21 @@ function useManipulateMyList({ id, type }) {
       .finally(() => {
         setLoading(false);
       });
+
+    // Compruebo si la película o serie está guardada en la lista del usuario actual
+    getItemFromMyList({ idItem: id, userId: currentUser.id })
+      .then((data) => {
+        if (data.length > 0) {
+          setSelected(true);
+        } else {
+          setSelected(false);
+        }
+      });
   }, [id]);
 
   const handleSaveMyList = async () => {
     // Añadir la película o serie a la base de datos
-    const newItem = await addItemToDB({
+    await addItemToDB({
       idItem: id,
       posterPath: itemDetail.poster_path,
       type,
@@ -36,7 +47,7 @@ function useManipulateMyList({ id, type }) {
     // Relacionar la película o serie añadida con el usuario actual
     await addItemToMyList({
       idUser: currentUser.id,
-      idItem: newItem[0].id,
+      idItem: id,
     });
 
     setSelected(true);
