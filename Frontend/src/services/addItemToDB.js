d8ac3b0ea841f5181f0fd61d1134a2d7abc8a754
Juan Antonio <jamll.muro@gmail.com>
2023-09-25 12:06:00 +0200

    Change the addItemToDB function to first check if the item already exists in the DB

--- Frontend/src/services/addItemToDB.js
+++ Frontend/src/services/addItemToDB.js
@@ -2,11 +2,18 @@ import supabase from '../../supabase';
 
 const addItemToDB = async (item) => {
   try {
-    const { data } = await supabase
+    // Compruebo si el nuevo item que se quiere insertar ya existe en la base de datos
+    const { data: exist } = await supabase
+      .from('movies_series')
+      .select('item_id')
+      .eq('item_id', item.item_id);
+
+    // Si ya existe no se vuelve a insertar
+    if (exist.length !== 0) return;
+
+    await supabase
       .from('movies_series')
       .insert(item);
-
-    return data;
   } catch (error) {
     throw new Error(error.message);
   }
