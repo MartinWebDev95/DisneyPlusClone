0c0d7968ae52a3c331d8c4a97978fa01ae62f7be
Juan Antonio <jamll.muro@gmail.com>
2023-09-25 11:55:37 +0200

    Change the query in getAllItemsFromMyList function

--- Frontend/src/services/getAllItemsFromMyList.js
+++ Frontend/src/services/getAllItemsFromMyList.js
@@ -4,8 +4,8 @@ const getAllItemsFromMyList = async ({ userId }) => {
   try {
     const { data } = await supabase
       .from('mylist')
-      .select('movies_series(idItem, posterPath, type)')
-      .eq('idUser', userId)
+      .select('movies_series(item_id, poster_path, type)')
+      .eq('user_id', userId)
       .order('created_at', { ascending: false });
 
     return data.map((item) => item.movies_series);
