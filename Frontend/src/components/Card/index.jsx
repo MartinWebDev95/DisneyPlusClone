8ce6d207b48b8c07f8d931e22c591475d703a420
Juan Antonio <jamll.muro@gmail.com>
2024-09-11 13:47:15 +0200

    Add the attribute draggable in false to the Card component

--- Frontend/src/components/Card/index.jsx
+++ Frontend/src/components/Card/index.jsx
@@ -6,7 +6,7 @@ function Card({
   item, type, width = '', height = '',
 }) {
   return (
-    <LinkStyled to={`/${type}/${item.id || item.item_id}`} width={width}>
+    <LinkStyled to={`/${type}/${item.id || item.item_id}`} width={width} draggable={false}>
       {item.poster_path !== null
         ? (
           <ImageStyled
@@ -14,6 +14,7 @@ function Card({
             alt={item.title || item.name}
             loading="lazy"
             height={height}
+            draggable={false}
           />
         ) : (
           <ImageStyled
@@ -21,6 +22,7 @@ function Card({
             alt="No available"
             loading="lazy"
             height={height}
+            draggable={false}
           />
         )}
     </LinkStyled>
