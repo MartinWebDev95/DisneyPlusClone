b21d5fb9af4eae1026a38d3dcbf117c771cca2a5
Juan Antonio Martín <jamll.muro@gmail.com>
2025-06-18 10:52:43 +0200

    Add a new file that return an array with the info of the different cards brand

---
+++ Frontend/src/utils/cardBrandData.js
@@ -0,0 +1,34 @@
+const cardBrandData = [
+  {
+    href: '/brand/disney',
+    image: '/assets/img/categories/disney.png',
+    video: '/assets/videos/disney.mp4',
+  },
+  {
+    href: '/brand/pixar',
+    image: '/assets/img/categories/pixar.png',
+    video: '/assets/videos/pixar.mp4',
+  },
+  {
+    href: '/brand/marvel',
+    image: '/assets/img/categories/marvel.png',
+    video: '/assets/videos/marvel.mp4',
+  },
+  {
+    href: '/brand/star-wars',
+    image: '/assets/img/categories/star_wars.png',
+    video: '/assets/videos/star-wars.mp4',
+  },
+  {
+    href: '/brand/national-geographic',
+    image: '/assets/img/categories/national_geographic.png',
+    video: '/assets/videos/national-geographic.mp4',
+  },
+  {
+    href: '/brand/star',
+    image: '/assets/img/categories/star.png',
+    video: '/assets/videos/brand-star.mp4',
+  },
+];
+
+export default cardBrandData;
